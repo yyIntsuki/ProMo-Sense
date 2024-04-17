ed5e8a6e963472c2eaa0c25d729402be73893c45
AliKazimovKth <149503857+AliKazimovKth@users.noreply.github.com>
2024-04-17 10:45:25 +0200

    MinorChanges
    Minor changes

--- View/aboutTheGroupMembers.jsx
+++ View/aboutTheGroupMembers.jsx
@@ -2,6 +2,63 @@ import React from "react";
 
 export default
 function aboutTheGroupMembers(props){
-    //function jump
+   
+    function goesToEmailACB(email){
+        window.location.href = `mailto:${email}`;
+    }
 
+    return(
+        <div className="About">
+           
+        <br />
+        <h2> Group 9 </h2>
+        <br />
+        <div className="about_row">
+        
+        <div className=" about_per1">
+            <h4>Salahudin Salah</h4><br />
+            <div className="about_per1_image"></div><br />
+            {   }
+            <p>Developer</p>
+            <p>sasalah@kth.se</p><br />
+            <p><button onClick={() => goesToEmailACB("kazimov@kth.se")} >Contact</button></p>
+        </div>
+        
+        <div className=" about_per2">
+            <h4>Ali Kazimov</h4><br />
+            <div className="about_per2_image"></div><br />
+            {   }
+            <p>Developer</p>
+            <p>kazimov@kth.se</p><br />
+            <p><button onClick={() => goesToEmailACB("kazimov@kth.se")} >Contact</button></p>
+        
+        
+        </div>
+        
+        <div className=" about_per3">
+            
+            <h4>Ludwig Ask</h4><br />
+            <div className="about_per3_image"></div><br />
+        
+            {    }
+            <p>Group Leader & Developer</p>
+            <p>gay.ll</p><br />
+            <p><button onClick={() => goesToEmailACB("kazimov@kth.se")} >Contact</button></p>
+        
+        </div>
+        
+        <div className=" about_per4">
+            <h4>Peter Li</h4><br />
+            <div className="about_per4_image"></div><br />
+            {   }
+            <p>Co Leader & Developer</p>
+            <p>ernman@kth.se</p><br />
+            <p><button onClick={() => goesToEmailACB("kazimov@kth.se")} >Contact</button></p>
+            
+        </div>
+        </div>
+        
+        </div>
+        
+            );
 }
---
+++ reactjs/aboutPresenter.jsx
@@ -0,0 +1,64 @@
+import aboutTheGroupMembers from "../View/aboutTheGroupMembers";
+import { observer } from "mobx-react-lite";
+import NavbarView from "../views/navBarView";
+import { auth } from "../firebaseModel";
+import { signInWithPopup, GoogleAuthProvider,signOut} from "firebase/auth";
+
+
+export default observer(
+    
+    function About(props){
+
+
+        function handleLoginACB(){
+            console.log(props);
+
+            const provider = new GoogleAuthProvider();
+            signInWithPopup(auth, provider)
+            .then((result) => {
+              console.log("Sign In User: ")
+              console.log(result)
+
+              const user = result.user;
+              props.props.UserState.user = user;
+              props.props.UserState.loginStatus = true;
+              console.log("LogIn successfully");
+          
+            }).catch((error) => {
+
+            }); 
+        }
+
+        function handleLogoutACB(){
+            signOut(auth).then(() => {
+
+                props.props.UserState.loginStatus = false;
+                props.props.UserState.user = null;
+                console.log("LogOut successfully");
+        
+              }).catch((error) => {
+                console.error(error);
+              });
+
+        }
+
+    return (<div>
+        
+        <div>
+        <NavbarView 
+            user = {props.props.UserState.user}
+            loginStatus = {props.props.UserState.loginStatus}
+            setLoginStatus = {handleLoginACB}   
+            setLogoutStatus = {handleLogoutACB}       
+            
+        />
+        </div>
+        <div>
+            <aboutTheGroupMembers></aboutTheGroupMembers>
+
+
+
+        </div>
+    </div>
+    );
+})
