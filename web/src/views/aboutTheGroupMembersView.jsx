645fc6138307cc167ce1c86d8b7d2ee93a1962b4
AliKazimovKth <149503857+AliKazimovKth@users.noreply.github.com>
2024-04-18 19:57:54 +0200

    Update aboutTheGroupMembersView.jsx
    Salahs view

--- web/src/views/aboutTheGroupMembersView.jsx
+++ web/src/views/aboutTheGroupMembersView.jsx
@@ -1,97 +1,54 @@
-import "/src/style.css";
 import React from "react";
+import "/src/style.css"; 
 
-
-function AboutTheGroupMembersView(props){
-   
-    function goesToEmailACB(email){
+function AboutTheGroupMembersView(props) {
+    function goesToEmailACB(email) {
         window.location.href = `mailto:${email}`;
     }
 
-    return(
-        <div className="About">
-           
-        <br />
-        <h2> Group 9 </h2>
-        <br />
-        <div className="about_row">
-        
-        <div className=" about_per1">
-            <h4>Salahudin Salah</h4><br />
-            <div className=""></div><br />
-            <div></div>
-    <img src="src/assets/SalahGrupp9.JPG" alt="salah" className="about_image"/>
-            <p>Developer</p>
-            <p>sasalah@kth.se</p><br />
-            <p><button onClick={() => goesToEmailACB("sasalah@kth.se")} >Contact</button></p>
-        </div>
-       
-        <div className=" about_per2">
-            <h4>Ali Kazimov</h4><br />
-            <div className=""></div><br />
-            <div></div>
-    <img src="src/assets/ALIGRUPP9.png" alt="salah" className="about_image"/>
-            <p>Developer</p>
-            <p>kazimov@kth.se</p><br />
-            <p><button onClick={() => goesToEmailACB("kazimov@kth.se")} >Contact</button></p>
-        
-        
-        </div>
-        
-        <div className=" about_per3">
-            
-            <h4>Ludwig Ask</h4><br />
-            <div className=""></div><br />
-        
-            <img src="src/assets/PeterGrupp9" alt="salah" className="about_image"/>
-            <p>Group Leader & Developer</p>
-            <p>ludwigas@kth.se</p><br />
-            <p><button onClick={() => goesToEmailACB("kazimov@kth.se")} >Contact</button></p>
-        
-        </div>
+    return (
+        <div className="about-group-members"> {}
+            <div className="about_per1">
+                <h4>Salahudin Salah</h4><br />
+                <img src="/assets/SalahGrupp9.JPG" alt="Salahudin Salah" className="about_image" /><br />
+                <p>Developer</p>
+                <p>sasalah@kth.se</p><br />
+                <p><button onClick={() => goesToEmailACB("sasalah@kth.se")}>Contact</button></p>
+            </div>
 
+            <div className="about_per2">
+                <h4>Ali Kazimov</h4><br />
+                <img src="/assets/ALIGRUPP9.png" alt="Ali Kazimov" className="about_image" /><br />
+                <p>Developer</p>
+                <p>kazimov@kth.se</p><br />
+                <p><button onClick={() => goesToEmailACB("kazimov@kth.se")}>Contact</button></p>
+            </div>
 
-         
-        <div className=" about_per4">
-            
-            <h4>pPeter Li</h4><br />
-            <div className=""></div><br />
-        
-            <img src="src/assets/D.jpg" alt="salah" className="about_image"/>
-            <p>Group Leader & Developer</p>
-            <p>ludwigas@kth.se</p><br />
-            <p><button onClick={() => goesToEmailACB("kazimov@kth.se")} >Contact</button></p>
-        
+            <div className="about_per3">
+                <h4>Ludwig Ask</h4><br />
+                <img src="/assets/PeterGrupp9.jpg" alt="Ludwig Ask" className="about_image" /><br />
+                <p>Group Leader & Developer</p>
+                <p>ludwigas@kth.se</p><br />
+                <p><button onClick={() => goesToEmailACB("kazimov@kth.se")}>Contact</button></p>
+            </div>
+
+            <div className="about_per4">
+                <h4>Peter Li</h4><br />
+                <img src="/assets/D.jpg" alt="Peter Li" className="about_image" /><br />
+                <p>Group Leader & Developer</p>
+                <p>97peter.l@gmail.com</p><br />
+                <p><button onClick={() => goesToEmailACB("kazimov@kth.se")}>Contact</button></p>
+            </div>
+
+            <div className="about_per5">
+                <h4>Daniel Lejonstad</h4><br />
+                <img src="/assets/DanielGrupp9.jpg" alt="Daniel Lejonstad" className="about_image" /><br />
+                <p>Co Leader & Developer</p>
+                <p>daniel.lejonstad@hotmail.com</p><br />
+                <p><button onClick={() => goesToEmailACB("kazimov@kth.se")}>Contact</button></p>
+            </div>
         </div>
-        
-        
-        <div className=" about_per4">
-            <h4>Peter Li</h4><br />
-            <div className = ""></div><br />
-            <img src = "src/assets/peterGrupp9.jpg"></img>
-            {   }
-            <p>Co Leader & Developer</p>
-            <p>97peter.l@gmail.com</p><br />
-            <p><button onClick={() => goesToEmailACB("kazimov@kth.se")} >Contact</button></p>
-            
-        </div>
-       
-       
-        <div className=" about_per5">
-            <h4>Daniel Lejonstad</h4><br />
-            <div className="about_per4_image"></div><br />
-            <img src = "src/assets/DanielGrupp9.jpg"></img>
-            {   }
-            <p>Scrum Master & Developer</p>
-            <p>daniel.lejonstad@hotmail.com</p><br />
-            <p><button onClick={() => goesToEmailACB("kazimov@kth.se")} >Contact</button></p>
-            
-        
-        </div>
-        </div>
-        </div>
-        
-            );
+    );
 }
 
 export default AboutTheGroupMembersView;
