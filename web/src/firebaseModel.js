f5bdacc942a142ac9e32988fdaf1a0fec9504cdc
Intsuki <intsuki.yz@gmail.com>
2024-04-22 20:15:45 +0200

    removed firestore dependency since we will not be using it.

--- web/src/firebaseModel.js
+++ web/src/firebaseModel.js
@@ -1,11 +1,9 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 import firebaseConfig from "./firebaseConfig";
 
 initializeApp(firebaseConfig);
 const auth = getAuth();
-const db = getFirestore();
 const provider = new GoogleAuthProvider();
 
-export { auth, db, provider, signInWithPopup, signOut }
+export { auth, provider, signInWithPopup, signOut }
