572df07fd75aa60856088a8e52c17ba609bfbe44
yyIntsuki <intsuki.yz@gmail.com>
2024-04-27 01:11:53 +0200

    added ability to track running time of the program. this way, the web app can track if raspberry is off or has issues.

--- raspberry/utils.py
+++ raspberry/utils.py
@@ -1,11 +1,16 @@
 import time
+import datetime
 
 
 def sleep(n):
     time.sleep(n)
 
 
-def get_time():
+def get_current_time():
     current_time = time.localtime()
     formatted_time = time.strftime("%m/%d/%Y, %H:%M:%S", current_time)
     return formatted_time
+
+
+def get_running_time(second):
+    return str(datetime.timedelta(seconds=second))
