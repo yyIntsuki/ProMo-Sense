20d5a0c787b53323c01c59577a5aa36f21f00b28
Salahudin77 <75946473+Salahudin77@users.noreply.github.com>
2024-04-27 10:30:43 +0200

    imported getDabase from firebase/database. This is done to save data to the database,

--- web/src/firebaseModel.js
+++ web/src/firebaseModel.js
@@ -1,8 +1,10 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithRedirect, getRedirectResult, signOut } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithRedirect, getRedirectResult, signOut, onAuthStateChanged } from "firebase/auth";
 import firebaseConfig from "./firebaseConfig";
+import { getDatabase } from "firebase/database";
 
 initializeApp(firebaseConfig);
 const auth = getAuth();
+const database = getDatabase();
 
-export { auth, GoogleAuthProvider, signInWithRedirect, getRedirectResult, signOut }
+export { auth, GoogleAuthProvider, signInWithRedirect, getRedirectResult, signOut, database, onAuthStateChanged }
