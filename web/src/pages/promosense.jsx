97809ce7e5323986ad75ee60c7b82ebf66b5bd86
Intsuki <intsuki.yz@gmail.com>
2024-04-27 16:23:14 +0200

    modified css styles for app.

--- web/src/pages/promosense.jsx
+++ web/src/pages/promosense.jsx
@@ -43,7 +43,7 @@ export default function App() {
 		pir_on: true,
 		pir_init: true,
 		pir_detect: true,
-		code_lock_status: true,
+		code_lock_status: false,
 		code_lock_remaining: '00:50',
 		code_lock_time: '5:00',
 		audio_sample: 'JohnCena',
@@ -52,45 +52,49 @@ export default function App() {
 
 	return (
 		<div className="app_wrapper">
-			<h1 className="app_status">Status</h1>
+			<div className='app_category'>
+				<h1>Status</h1>
 
-			<div className="item">
-				<h2 className="item_title">Motion sensor</h2>
-				<p>STATUS: {data.pir_on ? 'ON' : 'OFF'}</p>
-				<p>INITLIZATION: {data.pir_detect ? 'FINISHED' : 'PENDING'}</p>
-				<p>DETECTION: {data.pir_detect ? 'TRUE' : 'FALSE'}</p>
-			</div>
-
-			<div className="item">
-				<h2 className="item_title">Code-lock</h2>
-				<p>STATUS: {data.code_lock_status ? 'ACTIVE' : 'INACTIVE'}</p>
-				<p>REMAINING TIME: {data.code_lock_status ? `${data.code_lock_remaining} / ${data.code_lock_time}` : '-'}</p>
-			</div>
-
-			<h1 className="app_status">Control</h1>
-
-			<div className="item">
-				<audio ref={audioRef} preload="auto" hidden>Your browser does not support the audio element.</audio>
-				<h2 className="item_title">Audio module</h2>
-				<div className="item_detail">
-					<p>SAMPLE:</p>
-					<div className="detail_row">
-						<select onChange={handleAudioSelection} value={selectedAudioUrl}>
-							<option value="">Select a song</option>
-							<option value="https://firebasestorage.googleapis.com/v0/b/promosencegrupp9-4bbcc.appspot.com/o/Passionfruit.mp3?alt=media&token=5153bbfb-fcd0-4318-83a0-95c030adcd18">Passionfruit</option>
-							<option value="https://firebasestorage.googleapis.com/v0/b/promosencegrupp9-4bbcc.appspot.com/o/Drake%2C%20Kanye%20West%2C%20Lil%20Wayne%2C%20Eminem%20-%20Forever%20(Explicit%20Version)%20(Official%20Music%20Video).mp3?alt=media&token=610695c6-d26c-4ebd-878f-4c9609963167">Forever</option>
-							<option value="https://firebasestorage.googleapis.com/v0/b/promosencegrupp9-4bbcc.appspot.com/o/Indila%20-%20Tourner%20Dans%20Le%20Vide.mp3?alt=media&token=186cecf5-641c-41e8-a153-e03dafff267d">Bugatti</option>
-							<option value="https://firebasestorage.googleapis.com/v0/b/promosencegrupp9-4bbcc.appspot.com/o/Kanye%20West%20-%20Good%20Morning.mp3?alt=media&token=b3fb6f66-2653-42e0-801e-c252d9bdc3f0https://firebasestorage.googleapis.com/v0/b/promosencegrupp9-4bbcc.appspot.com/o/Kanye%20West%20-%20Good%20Morning.mp3?alt=media&token=b3fb6f66-2653-42e0-801e-c252d9bdc3f0">Good morning</option>
-						</select>
-						<button onClick={playAudio}>Test</button>
-					</div>
+				<div className="item">
+					<h2 className="item_title">Motion sensor</h2>
+					<p>STATUS: <span>{data.pir_on ? 'ON' : 'OFF'}</span></p>
+					<p>INITIALIZED: <span>{data.pir_detect ? 'FINISHED' : 'PENDING'}</span></p>
+					<p>DETECTED: <span>{data.pir_detect ? 'TRUE' : 'FALSE'}</span></p>
 				</div>
-				<div className='item_detail'>
-					<p>VOLUME:</p>
-					<div className="detail_row">
-						<button onClick={decreaseVolume}>-</button>
-						<span> {Math.round(volume * 100)} </span>
-						<button onClick={increaseVolume}>+</button>
+
+				<div className="item">
+					<h2 className="item_title">Code-lock</h2>
+					<p>STATUS: <span>{data.code_lock_status ? 'ACTIVE' : 'INACTIVE'}</span></p>
+					<p>REMAINING TIME: <span>{data.code_lock_status ? `${data.code_lock_remaining} / ${data.code_lock_time}` : '-'}</span></p>
+				</div>
+			</div>
+
+			<div className='app_category'>
+				<h1>Control</h1>
+
+				<div className="item">
+					<audio ref={audioRef} preload="auto" hidden>Your browser does not support the audio element.</audio>
+					<h2 className="item_title">Audio module</h2>
+					<div className="item_detail">
+						<p>SAMPLE:</p>
+						<div className="detail_row">
+							<select onChange={handleAudioSelection} value={selectedAudioUrl}>
+								<option value="">Select a song</option>
+								<option value="https://firebasestorage.googleapis.com/v0/b/promosencegrupp9-4bbcc.appspot.com/o/Passionfruit.mp3?alt=media&token=5153bbfb-fcd0-4318-83a0-95c030adcd18">Passionfruit</option>
+								<option value="https://firebasestorage.googleapis.com/v0/b/promosencegrupp9-4bbcc.appspot.com/o/Drake%2C%20Kanye%20West%2C%20Lil%20Wayne%2C%20Eminem%20-%20Forever%20(Explicit%20Version)%20(Official%20Music%20Video).mp3?alt=media&token=610695c6-d26c-4ebd-878f-4c9609963167">Forever</option>
+								<option value="https://firebasestorage.googleapis.com/v0/b/promosencegrupp9-4bbcc.appspot.com/o/Indila%20-%20Tourner%20Dans%20Le%20Vide.mp3?alt=media&token=186cecf5-641c-41e8-a153-e03dafff267d">Bugatti</option>
+								<option value="https://firebasestorage.googleapis.com/v0/b/promosencegrupp9-4bbcc.appspot.com/o/Kanye%20West%20-%20Good%20Morning.mp3?alt=media&token=b3fb6f66-2653-42e0-801e-c252d9bdc3f0https://firebasestorage.googleapis.com/v0/b/promosencegrupp9-4bbcc.appspot.com/o/Kanye%20West%20-%20Good%20Morning.mp3?alt=media&token=b3fb6f66-2653-42e0-801e-c252d9bdc3f0">Good morning</option>
+							</select>
+							<button onClick={playAudio}>Test</button>
+						</div>
+					</div>
+					<div className='item_detail'>
+						<p>VOLUME:</p>
+						<div className="detail_row">
+							<button onClick={decreaseVolume}>-</button>
+							<span> {Math.round(volume * 100)} </span>
+							<button onClick={increaseVolume}>+</button>
+						</div>
 					</div>
 				</div>
 			</div>
