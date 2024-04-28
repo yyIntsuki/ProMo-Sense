886a5ef3c7d34c7af3fb0e203fb641dc4bbba004
yyIntsuki <intsuki.yz@gmail.com>
2024-04-29 00:14:12 +0200

    refined footer navigation design and text less clutters and more mobile-friendly.

--- web/src/components/footerNav.jsx
+++ web/src/components/footerNav.jsx
@@ -3,15 +3,21 @@ import { useNavigate } from 'react-router-dom';
 
 export default function Footer() {
     const navigate = useNavigate();
-    function goToApp() { navigate('/'); }
-    function goToTeam() { navigate('/team'); }
-    function goToProject() { navigate('/project'); }
+    function navigateToApp() { navigate('/'); }
+    function navigateToProject() { navigate('/project'); }
+    function navigateToTeam() { navigate('/team'); }
 
     return (
         <div className="footer_wrapper">
-            <div onClick={goToApp}><h1>App</h1></div>
-            <div onClick={goToProject}><h1>The Project</h1></div>
-            <div onClick={goToTeam}><h1>The Team</h1></div>
+            <div onClick={navigateToApp}><h1>App</h1></div>
+            <div onClick={navigateToProject}>
+                <p>about</p>
+                <h1>Project</h1>
+            </div>
+            <div onClick={navigateToTeam}>
+                <p>about</p>
+                <h1>Team</h1>
+            </div>
         </div>
     )
 }
