30aa61ee2cd87473c641dccdaf9700fb47459ffd
Salahudin77 <75946473+Salahudin77@users.noreply.github.com>
2024-04-29 10:56:28 +0200

    Changed select song to select sound

--- web/src/pages/promosense.jsx
+++ web/src/pages/promosense.jsx
@@ -80,7 +80,7 @@ export default function App() {
                         <p>SAMPLE:</p>
                         <div className="detail_row">
                             <select onChange={handleAudioSelection} value={selectedAudioUrl}>
-                                <option value="">Select a song</option>
+                                <option value="">Select a sound</option>
                                 <option value="https://firebasestorage.googleapis.com/v0/b/promosencegrupp9-4bbcc.appspot.com/o/Passionfruit.mp3?alt=media&token=5153bbfb-fcd0-4318-83a0-95c030adcd18">Passionfruit</option>
                                 <option value="https://firebasestorage.googleapis.com/v0/b/promosencegrupp9-4bbcc.appspot.com/o/Drake%2C%20Kanye%20West%2C%20Lil%20Wayne%2C%20Eminem%20-%20Forever%20(Explicit%20Version)%20(Official%20Music%20Video).mp3?alt=media&token=610695c6-d26c-4ebd-878f-4c9609963167">Forever</option>
                                 <option value="https://firebasestorage.googleapis.com/v0/b/promosencegrupp9-4bbcc.appspot.com/o/Indila%20-%20Tourner%20Dans%20Le%20Vide.mp3?alt=media&token=186cecf5-641c-41e8-a153-e03dafff267d">Bugatti</option>
