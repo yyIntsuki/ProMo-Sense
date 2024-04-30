a97746b992a8fedd85dc8a1583141132429858a2
Intsuki <intsuki.yz@gmail.com>
2024-05-01 01:34:21 +0200

    added compressed and edited image links to about team page. should speed up the image load times significantly now.

--- web/src/pages/team.jsx
+++ web/src/pages/team.jsx
@@ -7,7 +7,7 @@ export default function Team() {
             <div className="about_group_members">
                 <div className="about_person">
                     <h4>Salahudin Salah</h4>
-                    <img src="https://i.ibb.co/ScX1q0M/Salah-Grupp9.jpg" alt="Salahudin Salah" />
+                    <img src="https://pli2.s-ul.eu/promosense/oVcgpMLz" alt="Salahudin Salah" />
                     <div className="roles">
                         <p>Product Owner</p>
                         <p>Developer</p>
@@ -20,7 +20,7 @@ export default function Team() {
 
                 <div className="about_person">
                     <h4>Ali Kazimov</h4>
-                    <img src="https://i.ibb.co/VwSjYgg/ALIGRUPP9.jpg" alt="Ali Kazimov" />
+                    <img src="https://pli2.s-ul.eu/promosense/EcmjYERi" alt="Ali Kazimov" />
                     <div className="roles">
                         <p>Developer</p>
                     </div>
@@ -32,7 +32,7 @@ export default function Team() {
 
                 <div className="about_person">
                     <h4>Ludwig Ask</h4>
-                    <img src="https://i.ibb.co/fxXDSFT/DIFSUGER.jpg" alt="Ludwig Ask" />
+                    <img src="https://pli2.s-ul.eu/promosense/RHATpVQx" alt="Ludwig Ask" />
                     <div className="roles">
                         <p>Developer</p>
                     </div>
@@ -44,7 +44,7 @@ export default function Team() {
 
                 <div className="about_person">
                     <h4>Peter Li</h4>
-                    <img src="https://i.ibb.co/P5JCfSM/peter-Grupp9.jpg" alt="Peter Li" />
+                    <img src="https://pli2.s-ul.eu/promosense/d7flcPFO" alt="Peter Li" />
                     <div className="roles">
                         <p>Developer</p>
                     </div>
@@ -56,7 +56,7 @@ export default function Team() {
 
                 <div className="about_person">
                     <h4>Daniel Lejonstad</h4>
-                    <img src="https://i.ibb.co/QQB53nd/Daniel-Grupp9.jpg" alt="Daniel Lejonstad" />
+                    <img src="https://pli2.s-ul.eu/promosense/n3cQN5I9" alt="Daniel Lejonstad" />
                     <div className="roles">
                         <p>Scrum Master</p>
                         <p>Developer</p>
