6d6d046e5edec138a7e53e3f64d641dde0244524
yyIntsuki <intsuki.yz@gmail.com>
2024-05-05 21:01:27 +0200

    audio program now selects randomly a file from user if not specified.

--- raspberry/src/audio.py
+++ raspberry/src/audio.py
@@ -2,12 +2,14 @@
 Module for handling audio playback from Raspberry Pi
 """
 
+from glob import glob
+from random import choice
 from pygame import mixer
 from firebase import get_files_from_storage, get_current_user, get_current_volume
 
 CURRENT_ACTIVE_USER = get_current_user()
 get_files_from_storage(CURRENT_ACTIVE_USER)
-SAMPLE_FILE_NAME = "And His Name is JOHN CENA - Sound Effect (HD).mp3"
+SAMPLE_FILE_NAME = None
 FILE_PATH = "../downloads/"
 
 
@@ -19,7 +21,14 @@ def init_audio():
 
 def load_audio():
     """Load a file into the pygame mixer music module"""
-    mixer.music.load(FILE_PATH + CURRENT_ACTIVE_USER + "/" + SAMPLE_FILE_NAME)
+    global SAMPLE_FILE_NAME
+
+    if not SAMPLE_FILE_NAME:
+        SAMPLE_FILE_NAME = choice(glob(f"{FILE_PATH}/{CURRENT_ACTIVE_USER}/*"))
+        mixer.music.load(SAMPLE_FILE_NAME)
+    else:
+        mixer.music.load(FILE_PATH + CURRENT_ACTIVE_USER + "/" + SAMPLE_FILE_NAME)
+
     print("Sample loaded.")
 
 
